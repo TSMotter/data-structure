699b0b5962b89eb798fefc4e2935c3dc54373075
Giuliano <mottergiuliano@gmail.com>
2023-02-01 15:38:57 -0300

    Add function to print 'root items' of the table (ie: items not redirected or pointed to by the underlying linked list;

--- hash_table/hash_table.c
+++ hash_table/hash_table.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define HASH_TABLE_SIZE 10
+#define HASH_TABLE_SIZE 3
 
 typedef struct HashTableEntry
 {
@@ -15,6 +16,32 @@ typedef struct HashTable
 {
   HashTableEntry *entries[HASH_TABLE_SIZE];
 } HashTable;
+
+void print_item(int index, struct HashTableEntry *item, const char *terminator)
+{
+  const char *pattern =
+      "table[%d] = (key: %s, value: %d, next: 0x%x) (@0x%x)%s";
+
+  if (item == NULL)
+  {
+    printf(pattern, index, "NULL", 0, 0, 0, terminator);
+  }
+  else
+  {
+    printf(pattern, index, item->key, item->value,
+           (item->next == NULL ? 0 : item->next), item, terminator);
+  }
+}
+
+// Insert a new key-value pair into the hash table
+void print_first_level_items(HashTable *table)
+{
+  for (int i = 0; i < HASH_TABLE_SIZE; i++)
+  {
+    print_item(i, table->entries[i], "\n");
+  }
+  printf("\n");
+}
 
 // Hash function that maps keys to indices in the array
 unsigned int hash(char *key)
@@ -40,12 +67,20 @@ HashTable *create_hash_table()
 }
 
 // Insert a new key-value pair into the hash table
-void hash_table_insert(HashTable *table, char *key, int value)
+void insert_item(HashTable *table, char *key, int value)
 {
   unsigned int    index = hash(key);
   HashTableEntry *entry = table->entries[index];
   while (entry != NULL && entry->key != NULL && strcmp(entry->key, key) != 0)
   {
+    printf("[WARN] Collision happened while trying to add item [key: %s, "
+           "value: %d]. The "
+           "position (%d) already contains the following item: [key: %s, "
+           "value: %d]. Will "
+           "make item [key: %s, value: %d] point to item [key: %s, value: %d] "
+           "on the field 'next'.\n",
+           key, value, index, entry->key, entry->value, key, value, entry->key,
+           entry->value);
     entry = entry->next;
   }
   if (entry == NULL)
@@ -63,7 +98,7 @@ void hash_table_insert(HashTable *table, char *key, int value)
 }
 
 // Retrieve the value for a given key from the hash table
-int hash_table_lookup(HashTable *table, char *key)
+int lookup_item(HashTable *table, char *key)
 {
   unsigned int    index = hash(key);
   HashTableEntry *entry = table->entries[index];
@@ -81,10 +116,17 @@ int hash_table_lookup(HashTable *table, char *key)
 int main()
 {
   HashTable *table = create_hash_table();
-  hash_table_insert(table, "key1", 1);
-  hash_table_insert(table, "key2", 2);
-  hash_table_insert(table, "key3", 3);
-  printf("Value for key 'key1': %d\n", hash_table_lookup(table, "key1"));
-  printf("Value for key 'key2': %d\n", hash_table_lookup(table, "key2"));
-  printf("Value for key 'key3': %d\n", hash_table_lookup(table, "key3"));
+  insert_item(table, "key10", 10);
+  insert_item(table, "key1", 1);
+  insert_item(table, "key2", 2);
+  insert_item(table, "key3", 3);
+  printf("Value for key 'key1': %d\n", lookup_item(table, "key1"));
+  printf("Value for key 'key2': %d\n", lookup_item(table, "key2"));
+  printf("Value for key 'key3': %d\n", lookup_item(table, "key3"));
+  printf("Value for key 'key10': %d\n", lookup_item(table, "key10"));
+
+  printf("Value for key 'invalid': %d\n", lookup_item(table, "invalid"));
+
+  // printf("This is address : 0x%x\n", table->entries[7]->next);
+  print_first_level_items(table);
 }
